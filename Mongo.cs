3fc8eafab83b0dbcc5a7ee84b518e2c08bd2c720
Paweł Pietrzyk <pa.pietrzyk@gmail.com>
2015-05-04 13:43:29 +0200

    Initial Commit

---
+++ Mongo.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Common.Mongo
+{
+    
+    [DataContract]
+    public class MongoId : INotifyPropertyChanged
+    {
+        private string _oid;
+        [DataMember(Name = "$oid")]
+        public string oid
+        {
+            get { return this._oid; }
+            set
+            {
+                this._oid = value;
+                OnPropertyChanged("oid");
+            }
+        }
+
+        public override string ToString()
+        {
+            return oid;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string prop)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }        
+        public object Clone()
+        {
+            MongoId id = new MongoId();
+            id.oid = this.oid;
+            return id;
+        }
+        public override int GetHashCode()
+        {
+            if (!String.IsNullOrEmpty(this.oid))
+            {
+                return this.oid.GetHashCode();
+            }
+            return base.GetHashCode();
+        }
+        public override bool Equals(object obj)
+        {
+            MongoId id = obj as MongoId;
+            if (id != null)
+            {
+                return String.Equals(id.oid, this.oid);
+            }
+            return false;
+        }
+    }
+    [DataContract]
+    public class MongoDocument : INotifyPropertyChanged
+    {
+        private MongoId id;
+        [DataMember(Name = "_id")]
+        public MongoId Id
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }        
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string prop)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+        public override string ToString()
+        {
+            if (Id != null)
+            {
+                return Id.ToString();
+            }
+            return String.Empty;
+        }
+
+        public virtual object Clone()
+        {
+            MongoDocument doc = new MongoDocument();
+            if (this.id != null)
+            {
+                doc.id = this.id.Clone() as MongoId;
+            }            
+            return doc;
+        }
+        public override bool Equals(object obj)
+        {
+            MongoDocument doc = obj as MongoDocument;
+            if (doc != null)
+            {
+                if (this.Id != null && doc.Id != null)
+                {
+                    return this.Id.Equals(doc.Id);
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ MongoDayOffs.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Common.Mongo
+{
+    public enum DayOffType
+    {
+        DayOff,
+        Balance        
+    }
+    [DataContract]
+    public class MongoDayOffSum : MongoDocument
+    {
+        public DateTime Updated
+        {
+            get
+            {
+                return DateTime.ParseExact(this.updatedTime, "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
+            }
+            set
+            {
+                this.updatedTime = value.ToString("yyyy-MM-dd HH:mm:ss");
+                OnPropertyChanged("UpdatedTime");
+            }
+        }
+
+        private string updatedTime;
+        [DataMember(Name = "updatedTime")]
+        public string UpdatedTime
+        {
+            get { return updatedTime; }
+            set
+            {
+                updatedTime = value;
+                this.Updated = this.Updated;
+            }
+        }
+
+        private int state;
+        [DataMember(Name = "state")]
+        public int State
+        {
+            get { return state; }
+            set
+            {
+                state = value;
+                OnPropertyChanged("State");
+            }
+        }
+
+    }
+
+    [DataContract]
+    public class MongoDayOff : MongoDocument
+    {
+        private bool edit;
+        [DataMember(Name = "edit")]
+        public bool Edit
+        {
+            get { return edit; }
+            set
+            {
+                edit = value;
+                OnPropertyChanged("Edit");
+            }
+        }
+
+        private DayOffType type;
+        [DataMember(Name = "type")]
+        public DayOffType Type
+        {
+            get { return type; }
+            set
+            {
+                type = value;
+                OnPropertyChanged("Type");
+            }
+        }
+        public DateTime Date
+        {
+            get
+            {
+                return DateTime.ParseExact(this.date, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+            }
+            set
+            {
+                this.date = value.ToString("yyyy-MM-dd");
+                OnPropertyChanged("Date");
+            }
+        }
+        private string date;
+        [DataMember(Name = "date")]
+        public string DateString
+        {
+            get { return date; }
+            set
+            {
+                date = value;
+                this.Date = this.Date;
+                OnPropertyChanged("DateString");
+            }
+        }        
+        private int state;
+        [DataMember(Name = "state")]
+        public int State
+        {
+            get { return state; }
+            set
+            {
+                state = value;
+                OnPropertyChanged("State");
+            }
+        }
+        public override object Clone()
+        {
+            MongoDayOff day = new MongoDayOff();
+            if (this.Id != null)
+            {
+                day.Id = this.Id.Clone() as MongoId;
+            }
+            day.state = this.state;
+            day.type = this.type;
+            day.date = this.date;
+            day.edit = this.edit;
+            return day;
+        }
+    }
+
+    public class MongoDayOffs : ObservableCollection<MongoDayOff>
+    {
+
+    }    
+}
